6e17bb1e2f5eb706f1eef2d646864fdf49bdabbd
Nachobear <kainerob@gmail.com>
2022-02-20 00:22:48 -0500

    Add project files.

---
+++ Lab 7/Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_7.Areas.Help.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+
+            if(id == "1")
+            {
+                return View("Page1");
+            }else if(id == "2")
+            {
+                return View("Page2");
+            }else
+            {
+                return View("Page3");
+            }
+            
+        }
+    }
+}
---
+++ Lab 7/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Phone = "(212)121-2121";
+            ViewBag.Email = "KaineRob@gmail.com";
+            ViewBag.Facebook = "www.facebook.com/nachobear";
+            return View();
+        }
+    }
+}
